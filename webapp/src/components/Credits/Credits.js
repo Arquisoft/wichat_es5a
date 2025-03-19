25af4801e76a371403f714c17c9b5d5390abdbf1
Tamargo <uo287694@uniovi.es>
2025-03-19 01:27:37 +0100

    Pequeños arreglos de interfaz

--- webapp/src/components/Credits/Credits.js
+++ webapp/src/components/Credits/Credits.js
@@ -3,42 +3,52 @@ import React from 'react';
 import LargeButton from '../ReactComponents/LargeButton';
 import { useNavigate } from 'react-router';
 import CustomH1 from '../ReactComponents/CustomH1';
+import Container from '@mui/material/Container';
 
 const Credits = () => {
 
   const navigate = useNavigate();
-  
+
   const exitCredits = () => {
     navigate('/home');
   }
 
   return (
-    <div>
-          <CustomH1>
-          Participantes
-          </CustomH1>
-          <CustomH1 size="h5">
-            UO288104@uniovi.es - Miguel Morís Gómez
-          </CustomH1>
-          <CustomH1 size="h5">
-          UO287694@uniovi.es - Pablo López Tamargo
-          </CustomH1>
-          <CustomH1 size="h5">
-          UO295432@uniovi.es - Óscar Cervero Luiña
-          </CustomH1>
-          <CustomH1 size="h5">
-          UO285412@uniovi.es - Néstor Fernández García
-          </CustomH1>
-          <CustomH1 size="h5">
-          gaelhorta04@gmail.com - Gael Horta Calzada
-          </CustomH1>
-          <CustomH1 size="h5">
-          UO295029@uniovi.es - Marcos Argüelles Rivera
-          </CustomH1>
-          <LargeButton onClick={exitCredits}>
-            Salir 
-          </ LargeButton>
-    </div>
+    <Container
+      component="main"
+      sx={{
+        marginTop: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <CustomH1>
+        Participantes
+      </CustomH1>
+      <CustomH1 size="h5">
+        UO288104@uniovi.es - Miguel Morís Gómez
+      </CustomH1>
+      <CustomH1 size="h5">
+        UO287694@uniovi.es - Pablo López Tamargo
+      </CustomH1>
+      <CustomH1 size="h5">
+        UO295432@uniovi.es - Óscar Cervero Luiña
+      </CustomH1>
+      <CustomH1 size="h5">
+        UO285412@uniovi.es - Néstor Fernández García
+      </CustomH1>
+      <CustomH1 size="h5">
+        gaelhorta04@gmail.com - Gael Horta Calzada
+      </CustomH1>
+      <CustomH1 size="h5">
+        UO295029@uniovi.es - Marcos Argüelles Rivera
+      </CustomH1>
+      <LargeButton onClick={exitCredits}>
+        Salir
+      </ LargeButton>
+    </Container>
   );
 };
 
--- webapp/src/components/Profile/Profile.js
+++ webapp/src/components/Profile/Profile.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 import LargeButton from '../ReactComponents/LargeButton';
 import CustomH1 from '../ReactComponents/CustomH1';
+import Container from '@mui/material/Container';
 
 const Profile = () => {
 
@@ -13,14 +14,23 @@ const Profile = () => {
   }
 
   return (
-    <div>
+    <Container
+      component="main"
+      sx={{
+        marginTop: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
           <CustomH1>
            AAAAAAAAAAAA
           </CustomH1>
           <LargeButton onClick={exitProfile}>
             Salir
           </LargeButton>
-    </div>
+    </Container>
   );
 };
 
