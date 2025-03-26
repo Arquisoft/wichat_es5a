8ebe37dde16f12739ecb32a155d7577d04957555
Marcos <Usuario@192.168.1.215>
2025-03-26 13:03:40 +0100

    Refactorización para evitar código duplicado v2

--- webapp/src/components/NavBar/NavBar.test.js
+++ webapp/src/components/NavBar/NavBar.test.js
@@ -4,58 +4,32 @@ import NavBar from './NavBar';
 import { BrowserRouter } from 'react-router'
 
 describe('NavBar component', () => {
+    
+    beforeEach(() => {
+        renderNavBar();
+    });
 
     it('should always pass', () => {
         expect(true).toBe(true);
     });
 
-    it('should render correctly the nav bar', () => {
-        renderNavBar();
-        expect(true).toBe(true);
-    });
+    const testCases = [
+        { tab: 'logo-tab', expectedPath: '/home' },
+        { tab: 'wichat-tab', expectedPath: '/home' },
+        { tab: 'profile-tab', expectedPath: '/profile' },
+        { tab: 'history-tab', expectedPath: '/history' },
+        { tab: 'credits-tab', expectedPath: '/credits' },
+        { tab: 'logout-tab', expectedPath: '/login' },
+    ];
 
-    it('should redirect to the home page when clicking on the app logo', async () => {
-        renderNavBar();
-
-        clickOn('logo-tab');
-        expect(window.location.pathname).toBe("/home");
-    });
-
-    it("should redirect to the home page when clicking on the app name", async () => {
-        renderNavBar();
-        
-        clickOn('wichat-tab');
-        expect(window.location.pathname).toBe("/home");
-    });
-
-    it("should redirect to the profile page when clicking on the profile tab", async () => {
-        renderNavBar();
-
-        clickOn('profile-tab');
-        expect(window.location.pathname).toBe("/profile");
-    });
-
-    it("should redirect to the history page when clicking on the history tab", async () => {
-        renderNavBar();
-        
-        clickOn('history-tab');
-        expect(window.location.pathname).toBe("/history");
-    });
-
-    it("should redirect to the credits page when clicking on the credits tab", async () => {
-        renderNavBar();
-        
-        clickOn('credits-tab');
-        expect(window.location.pathname).toBe("/credits");
-    });
-
-    it("should log out when clicking on the log out option", async () => {
-        renderNavBar();
-        
-        clickOn('logout-tab');
-        expect(window.location.pathname).toBe("/login");
+    testCases.forEach(({ tab, expectedPath }) => {
+        it(`should redirect to ${expectedPath} when clicking on ${tab}`, () => {
+            clickOn(tab);
+            expect(window.location.pathname).toBe(expectedPath);
+        });
     });
 });
+
 
 
 function renderNavBar() {
