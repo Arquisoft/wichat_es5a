15b5aee6df8b8026a9a113a454ad04de2aa7cabf
UO288104 <uo288104@uniovi.es>
2025-03-31 17:02:56 +0200

    Resueltos conflictos en user-service.js

--- users/userservice/user-service.js
+++ users/userservice/user-service.js
@@ -77,6 +77,7 @@ app.get('/profile', authenticateToken, async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
 app.get('/health', (req, res) => {
   res.json({ status: 'OK' });
 });
