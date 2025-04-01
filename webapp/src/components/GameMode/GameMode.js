260c1042b328d38c029c821e631e53913b05fd26
Marcos <uo295029@uniovi.es>
2025-04-01 20:38:58 +0200

    Añadida barra de navegación y cambiados colores de los botones

--- webapp/src/components/GameMode/GameMode.js
+++ webapp/src/components/GameMode/GameMode.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Box, Button, Typography, Grid } from '@mui/material';
 import { useNavigate } from 'react-router';
+import NavBar from "../NavBar/NavBar";
+import LargeButton from '../ReactComponents/LargeButton';
 
 const GameMode = () => {
   const navigate = useNavigate();
@@ -28,45 +30,66 @@ const GameMode = () => {
   };
 
   return (
-    <Box mt={4} textAlign="center">
-      <Typography variant="h4" gutterBottom>Selecciona el modo de juego</Typography>
-      <Grid container spacing={2} justifyContent="center">
-        {gameModes.map((mode) => (
-          <Grid item key={mode.value}>
-            <Button
-              variant={selectedMode === mode.value ? 'contained' : 'outlined'}
-              onClick={() => setSelectedMode(mode.value)}
-            >
-              {mode.label}
-            </Button>
-          </Grid>
-        ))}
-      </Grid>
+    <div>
+      <NavBar />
+      <Box mt={4} textAlign="center">
+        <Typography variant="h4" gutterBottom>Selecciona el modo de juego</Typography>
+        <Grid container spacing={2} justifyContent="center">
+          {gameModes.map((mode) => (
+            <Grid item key={mode.value}>
+              <Button
+                variant={selectedMode === mode.value ? 'contained' : 'outlined'}
+                onClick={() => setSelectedMode(mode.value)}
+                sx={{
+                  backgroundColor: selectedMode === mode.value ? "#606462" : "transparent",
+                  color: selectedMode === mode.value ? "white" : "#606462",
+                  borderColor: "#606462",
+                  "&:hover": {
+                    backgroundColor: selectedMode === mode.value ? "darkgray" : "rgba(128, 128, 128, 0.1)",
+                    borderColor: "darkgray"
+                  },
+                }}
+              >
+                {mode.label}
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
 
-      <Typography variant="h5" mt={4} gutterBottom>Selecciona la dificultad</Typography>
-      <Grid container spacing={2} justifyContent="center">
-        {difficulties.map((level) => (
-          <Grid item key={level}>
-            <Button
-              variant={selectedDifficulty === level ? 'contained' : 'outlined'}
-              onClick={() => setSelectedDifficulty(level)}
-            >
-              {level}
-            </Button>
-          </Grid>
-        ))}
-      </Grid>
+        <Typography variant="h5" mt={4} gutterBottom>Selecciona la dificultad</Typography>
+        <Grid container spacing={2} justifyContent="center">
+          {difficulties.map((level) => (
+            <Grid item key={level}>
+              <Button
+                variant={selectedDifficulty === level ? 'contained' : 'outlined'}
+                onClick={() => setSelectedDifficulty(level)}
+                sx={{
+                  backgroundColor: selectedDifficulty === level ? "#606462" : "transparent",
+                  color: selectedDifficulty === level ? "white" : "#606462",
+                  borderColor: "#606462",
+                  "&:hover": {
+                    backgroundColor: selectedDifficulty === level ? "darkgray" : "rgba(128, 128, 128, 0.1)",
+                    borderColor: "darkgray"
+                  },
+                }}
+              >
+                {level}
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
 
-      <Box mt={4}>
-        <Button
-          variant="contained"
-          disabled={!selectedMode || !selectedDifficulty}
-          onClick={startGame}
-        >
-          Empezar juego
-        </Button>
+        <Box mt={4}>
+          <LargeButton
+            variant="contained"
+            disabled={!selectedMode || !selectedDifficulty}
+            onClick={startGame}
+          >
+            Empezar juego
+          </LargeButton>
+        </Box>
       </Box>
-    </Box>
+    </div>
   );
 };
 
