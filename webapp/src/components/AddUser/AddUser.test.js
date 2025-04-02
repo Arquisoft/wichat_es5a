d36dd8bef8e25f62ee0f6e5dc8a1f44460d2215d
Marcos <uo295029@uniovi.es>
2025-04-02 21:03:59 +0200

    Añadido test de creación de usuario con éxito

--- webapp/src/components/AddUser/AddUser.test.js
+++ webapp/src/components/AddUser/AddUser.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import { BrowserRouter } from 'react-router';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import AddUser from './AddUser';
 
 const mockAxios = new MockAdapter(axios);
+
+function renderAddUser() {
+  render(
+    <BrowserRouter>
+      <AddUser/>
+    </BrowserRouter>
+  );
+}
 
 describe('AddUser component', () => {
 
@@ -12,34 +21,35 @@ describe('AddUser component', () => {
     expect(true).toBe(true);
   });
 
-  /*beforeEach(() => {
+  beforeEach(() => {
     mockAxios.reset();
   });
 
-  it('should add user successfully', async () => {
-    render(<AddUser />);
+  it('should render the add user formm correctly', async () => {
+    renderAddUser();
 
-    const usernameInput = screen.getByLabelText(/Username/i);
-    const passwordInput = screen.getByLabelText(/Password/i);
-    const addUserButton = screen.getByRole('button', { name: /Add User/i });
+    expect(screen.getByLabelText(/Nombre de usuario/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Contraseña/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Registrarse/i })).toBeInTheDocument();
+    expect(screen.getByText(/¿Ya tienes una cuenta/i)).toBeInTheDocument();
+  });
 
-    // Mock the axios.post request to simulate a successful response
-    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
+  it('should add a user successfully', async () => {
+    mockAxios.onPost('http://localhost:8000/addUser').reply(200);
 
-    // Simulate user input
-    fireEvent.change(usernameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
+    renderAddUser();
 
-    // Trigger the add user button click
-    fireEvent.click(addUserButton);
+    fireEvent.change(screen.getByLabelText(/Nombre de usuario/i), { target: { value: 'testUser' } });
+    fireEvent.change(screen.getByLabelText(/Contraseña/i), { target: { value: 'testPassword' } });
 
-    // Wait for the Snackbar to be open
+    fireEvent.click(screen.getByRole('button', { name: /Registrarse/i }));
+
     await waitFor(() => {
-      expect(screen.getByText(/User added successfully/i)).toBeInTheDocument();
+      expect(screen.getByText(/Usuario añadido con éxito/i)).toBeInTheDocument();
     });
   });
 
-  it('should handle error when adding user', async () => {
+  /*it('should handle error when adding user', async () => {
     render(<AddUser />);
 
     const usernameInput = screen.getByLabelText(/Username/i);
