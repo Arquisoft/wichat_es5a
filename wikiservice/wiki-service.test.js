a4a7a1b5d08cdcda3bcd6cf83fda06ae8e6c90d0
gaelhorta <gaelhorta04@gmail.com>
2025-04-02 20:59:49 +0200

    Arreglado test wikiservice

--- wikiservice/wiki-service.test.js
+++ wikiservice/wiki-service.test.js
@@ -24,16 +24,16 @@ afterAll(async () => {
     app.close(); 
 });
 
-describe('POST /questions', () => {
+describe('POST /questions/flag', () => {
     jest.setTimeout(20000);
 
-    it('should create a new question and return the correct format', async () => {
+    it('should create a new flag question and return the correct format', async () => {
         const response = await request(app)
-            .post('/questions')
+            .post('/questions/flag')
             .send();
 
         expect(response.statusCode).toBe(200);
-        expect(response.body).toHaveProperty('question', '¿Que ciudad es esta?');
+        expect(response.body).toHaveProperty('question', '¿De qué país es esta bandera?');
         expect(response.body).toHaveProperty('image');
         expect(response.body).toHaveProperty('answer');
         expect(response.body).toHaveProperty('wrongAnswers');
@@ -41,14 +41,14 @@ describe('POST /questions', () => {
         expect(response.body.wrongAnswers.length).toBe(3);
     });
 
-    it('should save the question in the database', async () => {
+    it('should save the flag question in the database', async () => {
         const response = await request(app)
-            .post('/questions')
+            .post('/questions/flag')
             .send();
 
-        const savedQuestion = await Question.findOne({ question: "¿Que ciudad es esta?" });
+        const savedQuestion = await Question.findOne({ question: "¿De qué país es esta bandera?" });
         expect(savedQuestion).toBeTruthy();
         expect(savedQuestion.answer).toBe(response.body.answer);
-        await Question.deleteOne({ question: "¿Que ciudad es esta?" });
+        await Question.deleteOne({ question: "¿Qué bandera es esta?" });
     });
 });
