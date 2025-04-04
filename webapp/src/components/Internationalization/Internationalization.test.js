8cc2927bbd5253c1356d90df4642bbbf32b50c67
Marcos <uo295029@uniovi.es>
2025-04-04 12:49:55 +0200

    Eliminado un warning de los test de internacionalización

--- webapp/src/components/Internationalization/Internationalization.test.js
+++ webapp/src/components/Internationalization/Internationalization.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router';  
 import Internationalization from './Internationalization.js';  
 import "../../i18n.js"
